5d85c4b4b5c71c1ec6e8d3eb252355da372cfc40
Gabi Viana <gabigviana@gmail.com>
2020-02-04 10:46:03 +0100

    deleted space

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function start() {
         const x = Math.random()*100;
         const y = Math.random()*100;
         const hue = Math.random() *360;
-        bubble.style.background = "hsl(" +hue+", 70%, 70%)";
+        bubble.style.background = "hsl("+hue+", 70%, 70%)";
         bubble.style.transform="translate("+x+"vw, "+y+"vh)";
     })
 }
